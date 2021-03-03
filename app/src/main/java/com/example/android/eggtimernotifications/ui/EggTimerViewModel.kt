978d084298034024769545f576ccccaf1a52aa7a
Keval <keval.langalia@live.com>
2021-03-03 20:43:19 +0530

    2.6 -> adding notification when timer is up in alarm receiver. and removed from start timer

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -118,12 +118,13 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 }
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
-                // TODO: Step 1.5 get an instance of NotificationManager and call sendNotification
-                val notificationmanager = ContextCompat.getSystemService(
-                    app,
-                    NotificationManager::class.java
-                ) as NotificationManager
-                notificationmanager.sendNotification(app.getString(R.string.timer_running), app)
+                // TODO_done: Step 1.5 get an instance of NotificationManager and call sendNotification
+                // previously used to just test the notification code. then moved to AlarmReceiver.
+//                val notificationmanager = ContextCompat.getSystemService(
+//                    app,
+//                    NotificationManager::class.java
+//                ) as NotificationManager
+//                notificationmanager.sendNotification(app.getString(R.string.timer_running), app)
 
                 // TODO: Step 1.15 call cancel notification
 
