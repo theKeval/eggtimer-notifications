5fd9af5aa3780e527b5ec1d20512b05ff7c4b590
Keval <keval.langalia@live.com>
2021-03-04 07:19:46 +0530

    2.12 -> style your notification

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -52,6 +52,10 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     )
 
     // TODO: Step 2.0 add style
+    val eggImage = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.cooked_egg)
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
 
     // TODO: Step 2.2 add snooze action
 
@@ -74,6 +78,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setAutoCancel(true)
 
     // TODO: Step 2.1 add style to builder
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
 
     // TODO: Step 2.3 add snooze action
 
